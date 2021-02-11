513797756e7d4d58480b92f5ed682912b7335ef3
FranciscoMendes10866 <avizinhadochico@gmail.com>
2021-02-12 00:15:11 +0100

    added footer with dynamic date

---
+++ components/footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <div className="container mx-auto py-14">
+            <p className="tracking-wider text-center text-md text-gray-400">
+                Copyright © {new Date().getFullYear()} Francisco Mendes. All rights reserved.
+            </p>
+        </div>
+    )
+}
+
+export default Footer
