d6eb56724dc8b4cb1e46c49993286451add8f6ff
FranciscoMendes10866 <avizinhadochico@gmail.com>
2021-02-12 04:06:27 +0100

    small changes

--- components/home/intro.jsx
+++ components/home/intro.jsx
@@ -1,3 +1,5 @@
+import { AiOutlineArrowDown } from 'react-icons/ai'
+
 const Intro = () => {
     return (
         <div className="container mx-auto px-10">
@@ -7,11 +9,12 @@ const Intro = () => {
                     <h1 className="text-5xl lg:text-7xl medium mt-2 md:mt-4 leading-tight md:leading-none">Full Stack Web Developer.</h1>
                     <p className="text-xl lg:text-3xl text-gray-500 light leading-loose mt-6 md:mt-8 lg:mt-10">
                         Born and raised in the beautiful Portuguese coast.
-                </p>
+                    </p>
                     <p className="text-xl lg:text-3xl text-gray-500 light leading-loose lg:leading-10 mt-0 md:mt-1 lg:mt-2">
                         My core value is to embrace my limitations and always
                         stay hungry for new challenges.
-                </p>
+                    </p>
+                    <AiOutlineArrowDown className="mt-10 text-gray-300 text-2xl ml-32" />
                 </div>
             </div>
         </div>
