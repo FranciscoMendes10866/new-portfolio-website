08a128a9a089f1bee05b43ac895aa4ab61d86de7
FranciscoMendes10866 <avizinhadochico@gmail.com>
2021-02-12 09:45:58 +0100

    small changes

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <div className="container mx-auto py-14">
-            <p className="tracking-wider text-center text-xs lg:text-base text-gray-300">
+            <p className="tracking-wider text-center text-xs lg:text-base text-gray-300 light">
                 Copyright © {new Date().getFullYear()} Francisco Mendes. All rights reserved.
             </p>
         </div>
